685493f3a252786d58f55490b7e52e83e3c0a5be
Niels <niels.muendler@inf.ethz.ch>
2026-06-04 20:14:36 +0200

    Cleanup

--- LwRust.lean
+++ LwRust.lean
@@ -1,6 +1,4 @@
 -- This module serves as the root of the `LwRust` library.
 -- Import modules here that should be built as part of the library.
 import LwRust.Definitions
-import LwRust.PartialProgram
 import LwRust.CompleteProgram
-import LwRust.Extractors.NestedBlocks
