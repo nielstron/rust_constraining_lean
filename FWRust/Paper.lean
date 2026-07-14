9cf41333effe2c108d314dee0c4db8e74c9abb72
niels <niels.muendler@inf.ethz.ch>
2026-07-14 20:50:12 +0200

    Remove redundant index

--- FWRust/Paper.lean
+++ FWRust/Paper.lean
@@ -3,5 +3,4 @@ import FWRust.Paper.Runtime
 import FWRust.Paper.InductiveSemantics
 import FWRust.Paper.Typing
 import FWRust.Paper.Soundness
-import FWRust.Paper.Soundness.Index
 import FWRust.Paper.Examples
